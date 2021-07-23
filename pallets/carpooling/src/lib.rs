0db8244f0fe52fcb5225879888c96115a7db3dea
Ankit Mogha <ankit.mogha@knoldus.com>
2021-07-24 02:38:37 +0530

    Add test cases

--- pallets/carpooling/src/lib.rs
+++ pallets/carpooling/src/lib.rs
@@ -3,7 +3,6 @@
 /// Edit this file to define custom logic or remove it if it is not needed.
 /// Learn more about FRAME and the core library of Substrate FRAME pallets:
 /// <https://substrate.dev/docs/en/knowledgebase/runtime/frame>
-
 pub use pallet::*;
 
 #[cfg(test)]
@@ -17,95 +16,98 @@ mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
-	use frame_system::pallet_prelude::*;
-	// CustomerOf is datatype used for storage in Customer
-	type CustomerOf<T> = SCustomer<<T as frame_system::Config>::Hash>;
-	// SCustomer is a struct for Customer
+    use frame_support::{dispatch::DispatchResult, pallet_prelude::*};
+    use frame_system::pallet_prelude::*;
+    // CustomerOf is datatype used for storage in Customer
+    type CustomerOf<T> = SCustomer<<T as frame_system::Config>::Hash>;
+    // SCustomer is a struct for Customer
     #[derive(Encode, Decode, Clone, Default, PartialEq, RuntimeDebug)]
     pub struct SCustomer<Hash> {
         pub id: u32,
         pub name: Hash,
         pub location: Hash,
     }
-	// DriverOf is datatype used for storage in Driver
-	type DriverOf<T> = SDriver<<T as frame_system::Config>::Hash>;
+    // DriverOf is datatype used for storage in Driver
+    type DriverOf<T> = SDriver<<T as frame_system::Config>::Hash>;
     //SDriver is a struct for Driver
-	#[derive(Encode, Decode, Clone, Default, PartialEq, RuntimeDebug)]
+    #[derive(Encode, Decode, Clone, Default, PartialEq, RuntimeDebug)]
     pub struct SDriver<Hash> {
         pub id: u32,
         pub car_no: Hash,
         pub location: Hash,
-		pub price: u32,
+        pub price: u32,
     }
-	/// Configure the pallet by specifying the parameters and types on which it depends.
-	#[pallet::config]
-	pub trait Config: frame_system::Config {
-		/// Because this pallet emits events, it depends on the runtime's definition of an event.
-		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-	}
+    /// Configure the pallet by specifying the parameters and types on which it depends.
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// Because this pallet emits events, it depends on the runtime's definition of an event.
+        type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+    }
 
-	#[pallet::pallet]
-	#[pallet::generate_store(pub(super) trait Store)]
-	pub struct Pallet<T>(_);
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn get_customer)]
+    // The pallet's runtime storage items.
+    // https://substrate.dev/docs/en/knowledgebase/runtime/storage
+    #[pallet::storage]
+    #[pallet::getter(fn get_customer)]
     pub type Customer<T: Config> = StorageMap<_, Blake2_128Concat, u32, CustomerOf<T>>;
-	#[pallet::storage]
+    #[pallet::storage]
     #[pallet::getter(fn get_driver)]
     pub type Driver<T: Config> = StorageMap<_, Blake2_128Concat, u32, DriverOf<T>>;
-	#[pallet::storage]
+    #[pallet::storage]
     #[pallet::getter(fn get_booking)]
     pub type Booking<T: Config> = StorageMap<_, Blake2_128Concat, u32, T::AccountId>;
-	// Learn more about declaring storage items:
-	// https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
+    // Learn more about declaring storage items:
+    // https://substrate.dev/docs/en/knowledgebase/runtime/storage#declaring-storage-items
 
-	// Pallets use events to inform users when important changes are made.
-	// https://substrate.dev/docs/en/knowledgebase/runtime/events
-	#[pallet::event]
-	#[pallet::metadata(T::AccountId = "AccountId")]
-	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		CustomerAdded(u32, T::AccountId),
-	}
+    // Pallets use events to inform users when important changes are made.
+    // https://substrate.dev/docs/en/knowledgebase/runtime/events
+    #[pallet::event]
+    #[pallet::metadata(T::AccountId = "AccountId")]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Event documentation should end with an array that provides descriptive names for event
+        /// parameters. [something, who]
+        CustomerAdded(u32, T::AccountId),
+    }
 
-	// Errors inform users that something went wrong.
-	#[pallet::error]
-	pub enum Error<T> {
-		/// Error names should be descriptive.
-		CustomerAlreadyExist,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
-	}
+    // Errors inform users that something went wrong.
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Error names should be descriptive.
+        CustomerAlreadyExist,
+        /// Errors should have helpful documentation associated with them.
+        StorageOverflow,
+    }
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
-	#[pallet::call]
-	impl<T:Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn add_new_customer(origin: OriginFor<T>, cust_id: u32, new_cust:CustomerOf<T>) -> DispatchResult {
-			
-			let who = ensure_signed(origin)?;
+    // Dispatchable functions allows users to interact with the pallet and invoke state changes.
+    // These functions materialize as "extrinsics", which are often compared to transactions.
+    // Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// An example dispatchable that takes a singles value as a parameter, writes the value to
+        /// storage and emits an event. This function must be dispatched by a signed extrinsic.
+        #[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+        pub fn add_new_customer(
+            origin: OriginFor<T>,
+            cust_id: u32,
+            new_cust: CustomerOf<T>,
+        ) -> DispatchResult {
+            let who = ensure_signed(origin)?;
 
-			match <Customer<T>>::get(cust_id) {
-				Some(_) => Err(Error::<T>::CustomerAlreadyExist)?,
-				None => {
-					<Customer<T>>::insert(cust_id, new_cust);
-				}
-			}
+            match <Customer<T>>::get(cust_id) {
+                Some(_) => Err(Error::<T>::CustomerAlreadyExist)?,
+                None => {
+                    <Customer<T>>::insert(cust_id, new_cust);
+                }
+            }
 
-			// Emit an event.
-			Self::deposit_event(Event::CustomerAdded(cust_id,who));
-			// Return a successful DispatchResultWithPostInfo
-			Ok(())
-		}
-	}
+            // Emit an event.
+            Self::deposit_event(Event::CustomerAdded(cust_id, who));
+            // Return a successful DispatchResultWithPostInfo
+            Ok(())
+        }
+    }
 }
--- pallets/carpooling/src/tests.rs
+++ pallets/carpooling/src/tests.rs
@@ -1,20 +1,70 @@
-use crate::{Error, mock::*};
-use crate::SCustomer;
+use crate::{mock::*, SCustomer};
+use frame_support::assert_ok;
+use frame_support::sp_runtime::DispatchError::Module;
 use sp_core::Encode;
 use sp_runtime::traits::Hash;
-use frame_support::{assert_ok, assert_noop};
 
 #[test]
 fn add_new_customer() {
-	new_test_ext().execute_with(|| {
-		let new_cust = SCustomer {
-			id:32,
-			name: "Ankit".using_encoded(<Test as frame_system::Config>::Hashing::hash),
-			location: "New Delhi".using_encoded(<Test as frame_system::Config>::Hashing::hash),
-		};
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::add_new_customer(Origin::signed(1), 42, new_cust));
-		// Read pallet storage and assert an expected result.
-		//assert_eq!(TemplateModule::get_customer(42), Some(&new_cust));
-	});
+    new_test_ext().execute_with(|| {
+        let new_cust = SCustomer {
+            id: 32,
+            name: "Ankit".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: "New Delhi".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+        };
+        // Dispatch a signed extrinsic.
+        assert_eq!(
+            TemplateModule::add_new_customer(Origin::signed(1), 42, new_cust),
+            Ok(())
+        );
+        // Read pallet storage and assert an expected result.
+    });
+}
+
+#[test]
+fn add_new_customer_fails() {
+    new_test_ext().execute_with(|| {
+        let new_cust = SCustomer {
+            id: 32,
+            name: "Ankit".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: "New Delhi".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+        };
+        // Dispatch a signed extrinsic.
+        assert_ok!(TemplateModule::add_new_customer(
+            Origin::signed(1),
+            42,
+            new_cust
+        ));
+        let new_cust_1 = SCustomer {
+            id: 32,
+            name: "Ankit".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: "New Delhi".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+        };
+        assert_eq!(
+            TemplateModule::add_new_customer(Origin::signed(1), 42, new_cust_1),
+            Err(Module {
+                index: 1,
+                error: 0,
+                message: Some("CustomerAlreadyExist")
+            })
+        );
+    });
+}
+
+#[test]
+fn check_storage() {
+    new_test_ext().execute_with(|| {
+        let new_cust = SCustomer {
+            id: 32,
+            name: "Ankit".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: "New Delhi".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+        };
+        let new_cust_1 = new_cust.clone();
+        assert_ok!(TemplateModule::add_new_customer(
+            Origin::signed(1),
+            42,
+            new_cust
+        ));
+        assert_eq!(TemplateModule::get_customer(42), Some(new_cust_1));
+    })
 }
