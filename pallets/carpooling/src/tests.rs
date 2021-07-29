f8fc434e826c0c7b8ed6942b0bf68112c982600c
Ayushi <ayushi.dwivedi@knoldus.com>
2021-07-29 16:28:27 +0530

    resolved merge conflicts

--- pallets/carpooling/src/tests.rs
+++ pallets/carpooling/src/tests.rs
@@ -57,3 +57,47 @@ fn update_cab_location_fail() {
         )
     });
 }
+#[test]
+fn add_new_cab() {
+    new_test_ext().execute_with(|| {
+        let new_cab = SDriver {
+            id: 32,
+            car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: (20, 30),
+            price: 12,
+        };
+        // Dispatch a signed extrinsic.
+        assert_eq!(
+            Carpooling::add_new_cab(Origin::signed(1), 42, new_cab),
+            Ok(())
+        );
+        // Read pallet storage and assert an expected result.
+    });
+}
+#[test]
+fn add_new_cab_fails() {
+    new_test_ext().execute_with(|| {
+        let new_cab = SDriver {
+            id: 32,
+            car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: (20, 30),
+            price: 12,
+        };
+        // Dispatch a signed extrinsic.
+        assert_ok!(Carpooling::add_new_cab(Origin::signed(1), 42, new_cab));
+        let new_cab_1 = SDriver {
+            id: 32,
+            car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: (20, 30),
+            price: 12,
+        };
+        assert_eq!(
+            Carpooling::add_new_cab(Origin::signed(1), 42, new_cab_1),
+            Err(DispatchError::Module {
+                index: 1,
+                error: 1,
+                message: Some("CabAlreadyExist")
+            })
+        );
+    });
+}
