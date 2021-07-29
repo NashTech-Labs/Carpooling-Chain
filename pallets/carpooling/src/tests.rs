066fe66b5bcc066d7fbe828cf845294fb38d9233
Ayushi Dwivedi <85158906+Ayushi-knoldus@users.noreply.github.com>
2021-07-29 15:30:46 +0530

    Update tests.rs

--- pallets/carpooling/src/tests.rs
+++ pallets/carpooling/src/tests.rs
@@ -12,7 +12,7 @@ fn add_new_cab() {
             id: 32,
             car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
             location: (20, 30),
-			price: 12,
+            price: 12,
         };
         // Dispatch a signed extrinsic.
         assert_eq!(
@@ -26,10 +26,10 @@ fn add_new_cab() {
 fn add_new_cab_fails() {
     new_test_ext().execute_with(|| {
         let new_cab = SDriver {
-			id: 32,
+            id: 32,
             car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
             location: (20, 30),
-			price: 12,
+            price: 12,
         };
         // Dispatch a signed extrinsic.
         assert_ok!(Carpooling::add_new_cab(Origin::signed(1), 42, new_cab));
@@ -37,23 +37,68 @@ fn add_new_cab_fails() {
             id: 32,
             car_no: "2345".using_encoded(<Test as frame_system::Config>::Hashing::hash),
             location: (20, 30),
-			price: 12,
+            price: 12,
         };
-        assert_eq!(Carpooling::add_new_cab(Origin::signed(1), 42, new_cab_1) ,
-            Err(DispatchError::Module {index: 1,error: 0,message: Some("CabAlreadyExist")})
+        assert_eq!(
+            Carpooling::add_new_cab(Origin::signed(1), 42, new_cab_1),
+            Err(DispatchError::Module {
+                index: 1,
+                error: 0,
+                message: Some("CabAlreadyExist")
+            })
         );
     });
 }
-/*
 
 #[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			TemplateModule::cause_error(Origin::signed(1)),
-			Error::<Test>::NoneValue
-		);
-	});
+fn check_storage() {
+    new_test_ext().execute_with(|| {
+        let cust = SCustomer {
+            id: 20,
+            name: "aman".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: (20, 30),
+        };
+        Customer::<Test>::insert(10, &cust);
+        assert_eq!(<Customer<Test>>::get(10), Some(cust));
+    });
 }
-*/
+
+#[test]
+fn update_cab_location_success() {
+    new_test_ext().execute_with(|| {
+        let old_driver = SDriver {
+            id: 20,
+            car_no: "UP76 E 8559".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: (20, 30),
+            price: 20,
+        };
+        let new_location: (u32, u32) = (40, 40);
+        let new_driver = SDriver {
+            id: 20,
+            car_no: "UP76 E 8559".using_encoded(<Test as frame_system::Config>::Hashing::hash),
+            location: new_location,
+            price: 20,
+        };
+        Driver::<Test>::insert(10, old_driver);
+        assert_ok!(Carpooling::update_cab_location(
+            Origin::signed(10),
+            10,
+            new_location
+        ));
+        assert_eq!(<Driver<Test>>::get(10), Some(new_driver));
+    });
+}
+#[test]
+fn update_cab_location_fail() {
+    new_test_ext().execute_with(|| {
+        let new_location: (u32, u32) = (40, 50);
+        assert_eq!(
+            Carpooling::update_cab_location(Origin::signed(1), 42, new_location),
+            Err(DispatchError::Module {
+                index: 1,
+                error: 2,
+                message: Some("DriverDoesNotExist",),
+            })
+        )
+    });
+}
