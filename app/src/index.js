df06559e8059cdfde50e643bab4cb07c76c8b0cd
Ayushi <ayushi.dwivedi@knoldus.com>
2021-09-05 00:11:27 +0530

    CC -SP4- 101.8.4 - Polkadot API for update customer location

--- app/src/index.js
+++ app/src/index.js
@@ -59,4 +59,53 @@ app.get('/index', (req, res) =>{
     res.send("Done");
 });
 
+app.get('/updatecust', (req, res) =>{
+
+    // digestMessage function converts a string to H256 hash string.
+    //
+    // # Arguments
+    //
+    // * `message` - A string parameter containing data to be converted.
+    //
+    // # Return
+    //
+    // A string containing hash value.
+    async function digestMessage(message) {
+        try{
+            const hash = await crypto.createHash('sha256',message).digest('hex');
+            return hash;
+        }
+
+        catch(error){
+            console.log(error);
+        }
+
+      }
+    // main functions calls the updatecustlocation dispatch function to update customer location.
+    async function main(){
+
+
+        // Construct
+        const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+        const keyring = new Keyring({ type: 'sr25519' });
+        const alice = keyring.addFromUri('//Alice');
+        const types = JSON.parse(readFileSync('./types.json', 'utf8'));
+        const api = await ApiPromise.create({ provider: wsProvider,
+            types
+        });
+
+        try{
+                const updateCustLocation = api.tx.carpooling.updateCustLocation(15,{id: 15, name: "0x"+cust, location: [40,20]});
+                updateCustLocation.signAndSend(alice);
+                console.log(`The customer location was updated successfully`);
+            ;
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    main().then(() => console.log('completed'));
+    res.send("Done");
+});
 app.listen(6069);
