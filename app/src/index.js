ee2ded9f4cfb74f19d197242d8547e5657a8d9a9
ankit-mogha <ankit.mogha@knoldus.com>
2021-09-06 20:56:45 +0530

    CC-SP4-101.8.6 Added API for making cab idle

--- app/src/index.js
+++ app/src/index.js
@@ -19,7 +19,7 @@ app.get('/index', (req, res) =>{
     //
     // A string containing hash value.
     async function digestMessage(message) {
-        try{            
+        try{
             const hash = await crypto.createHash('sha256',message).digest('hex');
             return hash;
         }
@@ -27,12 +27,12 @@ app.get('/index', (req, res) =>{
         catch(error){
             console.log(error);
         }
-        
+
       }
     // main functions calls the addNewCab dispatch function to add a new cab.
     async function main(){
-    
-    
+
+
         // Construct
         const wsProvider = new WsProvider('ws://127.0.0.1:9944');
         const keyring = new Keyring({ type: 'sr25519' });
@@ -41,7 +41,7 @@ app.get('/index', (req, res) =>{
         const api = await ApiPromise.create({ provider: wsProvider,
             types
         });
-        
+
         try{
             let namePromise = digestMessage("Aman Verma");
             namePromise.then((cust)=>{
@@ -49,12 +49,42 @@ app.get('/index', (req, res) =>{
                 addCust.signAndSend(alice);
                 console.log(`The customer was successfully added`);
             });
-            
+
         }
         catch(error){
             console.log(error);
-        }  
-    }   
+        }
+    }
+    main().then(() => console.log('completed'));
+    res.send("Done");
+});
+
+app.get('/make-cab-idle', (req, res) =>{
+    // main functions calls the makeCabIdle dispatch function to add a new cab.
+    async function main(){
+
+
+        // Construct
+        const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+        const keyring = new Keyring({ type: 'sr25519' });
+        const alice = keyring.addFromUri('//Alice');
+        const types = JSON.parse(readFileSync('./types.json', 'utf8'));
+        const api = await ApiPromise.create({ provider: wsProvider,
+            types
+        });
+
+        try{
+            {
+                const makeCabIdle = api.tx.carpooling.makeCabIdle(15);
+                makeCabIdle.signAndSend(alice);
+                console.log(`The cab was made Idle Successfully`);
+            }
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
     main().then(() => console.log('completed'));
     res.send("Done");
 });
