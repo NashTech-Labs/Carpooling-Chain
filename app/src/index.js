673a743baa66da136e4eeff88ce6c775d82ffa19
ankit-mogha <ankit.mogha@knoldus.com>
2021-09-06 20:13:03 +0530

    Resloved Merge Conflicts

--- app/src/index.js
+++ app/src/index.js
@@ -7,8 +7,60 @@ var crypto = require('crypto');
 
 const app = express();
 
-app.get('/index', (req, res) =>{
+app.get('/add-customer', (req, res) =>{
+
+    // digestMessage function converts a string to H256 hash string.
+    //
+    // # Arguments
+    //
+    // * `message` - A string parameter containing data to be converted.
+    //
+    // # Return
+    //
+    // A string containing hash value.
+    async function digestMessage(message) {
+        try{
+            const hash = await crypto.createHash('sha256',message).digest('hex');
+            return hash;
+        }
+
+        catch(error){
+            console.log(error);
+        }
+
+      }
     // main functions calls the addNewCab dispatch function to add a new cab.
+    async function main(){
+
+
+        // Construct
+        const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+        const keyring = new Keyring({ type: 'sr25519' });
+        const alice = keyring.addFromUri('//Alice');
+        const types = JSON.parse(readFileSync('./types.json', 'utf8'));
+        const api = await ApiPromise.create({ provider: wsProvider,
+            types
+        });
+
+        try{
+            let namePromise = digestMessage("Aman Verma");
+            namePromise.then((cust)=>{
+                const addCust = api.tx.carpooling.addNewCustomer(15,{id: 15, name: "0x"+cust, location: [20,40]});
+                addCust.signAndSend(alice);
+                console.log(`The customer was successfully added`);
+            });
+
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    main().then(() => console.log('completed'));
+    res.send("Done");
+});
+
+app.get('/update-cab-location', (req, res) =>{
+    // main functions calls the updateCabLocation dispatch function to add a new cab.
     async function main(){
 
         // Construct
