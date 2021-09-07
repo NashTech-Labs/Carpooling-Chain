99389da7f881fdbf782a8b67439c6c35cb362c39
ankit-mogha <ankit.mogha@knoldus.com>
2021-09-07 18:57:46 +0530

    Resloved Merge Conflicts

--- app/src/index.js
+++ app/src/index.js
@@ -59,6 +59,56 @@ app.get('/index', (req, res) =>{
     res.send("Done");
 });
 
+app.get('/update-cab-location', (req, res) =>{
+    // main functions calls the updateCabLocation dispatch function to add a new cab.
+    async function main(){
+
+        // Construct
+        const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+        const keyring = new Keyring({ type: 'sr25519' });
+        const alice = keyring.addFromUri('//Alice');
+        const types = JSON.parse(readFileSync('./types.json', 'utf8'));
+        const api = await ApiPromise.create({ provider: wsProvider,
+            types
+        });
+
+        try{
+                const updateCabLocation = api.tx.carpooling.updateCabLocation(15,[20,40]);
+                updateCabLocation.signAndSend(alice);
+                console.log(`The cab location was updated successfully`);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    main().then(() => console.log('completed'));
+    res.send("Done");
+});
+
+app.get('/book', (req, res) =>{
+    // main functions calls the bookRide dispatch function to book a cab.
+    async function main(){
+        // Construct
+        const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+        const keyring = new Keyring({ type: 'sr25519' });
+        const alice = keyring.addFromUri('//Alice');
+        const types = JSON.parse(readFileSync('./types.json', 'utf8'));
+        const api = await ApiPromise.create({ provider: wsProvider,
+            types
+        });
+        try{
+            const booked = api.tx.carpooling.bookRide(15, 30);
+            booked.signAndSend(alice);
+            console.log(`The cab was successfully booked`);
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    main().then(() => console.log('completed'));
+    res.send("Done");
+});
+
 app.get('/make-cab-idle', (req, res) =>{
     // main functions calls the makeCabIdle dispatch function to add a new cab.
     async function main(){
